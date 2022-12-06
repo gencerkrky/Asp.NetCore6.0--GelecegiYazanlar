f673fa488dbc72252d66602218c47585f2c400a9
Gencer KARAKAYA <gencerkrky@gmail.com>
2022-12-06 13:35:58 +0300

    Form Elements

--- Asp.NetCore6.0/Models/Product.cs
+++ Asp.NetCore6.0/Models/Product.cs
@@ -7,6 +7,8 @@
         public string? Name { get; set; }
         public decimal Price { get; set; }
         public int Stock { get; set; }
+
+        public string Description { get; set; }
         public string? Color { get; set; }
 
         public bool IsPublish { get; set; }
