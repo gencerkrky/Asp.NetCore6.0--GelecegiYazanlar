c26045002c8f1088062bcc94ed6226e3b85dab59
Gencer KARAKAYA <gencerkrky@gmail.com>
2022-12-11 12:03:34 +0300

    ViewComponent

---
+++ Asp.NetCore6.0/ViewModel/ProductListComponentViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Asp.NetCore6._0.ViewModel
+{
+    public class ProductListComponentViewModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ Asp.NetCore6.0/Views/Shared/ViewComponents/ProductListViewComponent.cs
@@ -0,0 +1,41 @@
+﻿using Asp.NetCore6._0.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+using Turkcell.Models;
+
+namespace Asp.NetCore6._0.Views.Shared.ViewComponents
+{
+    public class ProductListViewComponent:ViewComponent
+    {
+
+        private readonly AppDbContext _context;
+
+        public ProductListViewComponent(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(int type=1)
+        {
+
+            var viewmodels=_context.Products.Select(x=> new ProductListComponentViewModel()
+            {
+                Name=x.Name,
+                Description=x.Description,
+            }).ToList();
+
+            if (type==1)
+            {
+                return View("Default",viewmodels);
+            }
+            else { 
+                return View("Type2",viewmodels);
+
+            }
+
+
+        }
+
+
+
+    }
+}
